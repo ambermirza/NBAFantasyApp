ea836ed7c3ccd86cb3445d233292c4ce95f45c37
Amber Mirza <ambermirza89@gmail.com>
2017-09-29 20:05:27 -0700

    nm

--- NBAFantasyApp/ViewController.swift
+++ NBAFantasyApp/ViewController.swift
@@ -5,21 +5,19 @@
 //  Created by Amber Mirza on 9/29/17.
 //  Copyright © 2017 Amber Mirza. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
